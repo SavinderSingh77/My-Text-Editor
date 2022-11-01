851a2ee9e8c804c1d7f2d4704c589d0ab6d671be
Savinder <savindersingh500@gmail.com>
2022-11-01 13:06:01 +0530

    btns,wave and bg improved

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -90,47 +90,47 @@ const bgClick = ()=>{
           value={text}
           onClick = {bgClick}
           onChange={handleChnage}
-          style = {{background: `rgba(255, 255, 255, 0.23)`}}
+          style = {{background: `rgba(255, 255, 255, 0.23)`,color:'#000000bf',fontSize:'1.3rem'}}
         ></textarea>
         <button
-          onMouseEnter={upperCaseClick}
+          onClick={upperCaseClick}
           type="button"
-          className="btn btn-primary my-3 mx-2"
+          className="btn button-style my-3 mx-2"
         >
           Convert to Upper Case
         </button>
         <button
           onClick={lowerCaseClick}
           type="button"
-          className="btn btn-primary my-3 mx-2"
+          className="btn button-style my-3 mx-2"
         >
           Convert to Lower Case
         </button>
         <button
           onClick={titleCaseClick}
           type="button"
-          className="btn btn-primary my-3 mx-2"
+          className="btn button-style my-3 mx-2"
         >
           Convert to Tilte Case
         </button>
         <button
           onClick={clearTextClick}
           type="button"
-          className="btn btn-primary my-3 mx-2"
+          className="btn button-style my-3 mx-2"
         >
           Clear Text
         </button>
         <button
           onClick={copyTextClick}
           type="button"
-          className="btn btn-primary my-3 mx-2"
+          className="btn button-style my-3 mx-2"
         >
           Copy to Clipboard
         </button>
         <button
           onClick={removeSpacesClick}
           type="button"
-          className="btn btn-primary my-3 mx-2"
+          className="btn button-style my-3 mx-2"
         >
           Remove Extra Spaces
         </button>
@@ -143,11 +143,10 @@ const bgClick = ()=>{
             ? "Enter some text in the textbox to preview it here"
             : text}
         </p>
-   
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-  <path fill="#000b76" fill-opacity="1" d="M0,192L34.3,170.7C68.6,149,137,107,206,112C274.3,117,343,171,411,208C480,245,549,267,617,234.7C685.7,203,754,117,823,117.3C891.4,117,960,203,1029,240C1097.1,277,1166,267,1234,218.7C1302.9,171,1371,85,1406,42.7L1440,0L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
-</svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2c296e" fill-opacity="1" d="M0,224L40,229.3C80,235,160,245,240,218.7C320,192,400,128,480,101.3C560,75,640,85,720,122.7C800,160,880,224,960,245.3C1040,267,1120,245,1200,202.7C1280,160,1360,96,1400,64L1440,32L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+      <div id = "bg-svg"><footer > Copyright &#169; 2022 SaviSingh.com </footer></div>
+
     </>
   );
 }
